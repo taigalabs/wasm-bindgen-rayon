3dca3bd1063beeb6e62b4e8a699ca23b7c940d91
Elden Park <eldeniyenden@gmail.com>
2023-06-27 18:55:52 +0900

    o

---
+++ demo/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 4000;
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
