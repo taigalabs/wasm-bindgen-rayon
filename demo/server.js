c7edf0dac1a0876fee82723ec475447094a21d17
Elden Park <eldeniyenden@gmail.com>
2023-10-09 13:07:17 +0900

    o

--- demo/server.js
+++ demo/server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("dist"));
+app.use(
+  express.static("dist", {
+    setHeaders: (res, _path) => {
+      res.set("Cross-Origin-Embedder-Policy", "require-corp");
+      res.set("Cross-Origin-Opener-Policy", "same-origin");
+    },
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
